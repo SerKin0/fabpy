1f1b435bb6ecaffb05870753a1ff23ad0ad2882d
SerKin0 <serget.skor007@gmail.com>
2025-03-14 22:44:34 +0300

    first

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from fabpy.absolute import StandardDeviation, RandomError, IntsrumentalError, AbsoluteError
+from fabpy.indirect import IndetectError
---
+++ indirect.py
@@ -0,0 +1,30 @@
+from math import sqrt
+from dataclasses import dataclass
+
+from fabpy.constants import students_coefficient
+from fabpy.utils import rounding, student
+
+class IndetectError:
+    def __init__(self, data: dict[tuple], name: str = 't', roundoff: int = 1, floating_point: str = ','):
+        self.data = data
+        self.name = name
+        self.roundoff = roundoff
+        self.floating_point = floating_point
+
+        self.latex_name = str()
+        self.latex_general = str()
+        self.latex_values = str()
+        self.latex_result = str()
+
+        self.check_values = False
+        self.check_latex = False
+
+    def calculation(self):
+        pass
+
+    def build(self):
+        pass
+
+    def latex(self):
+        pass
+    
