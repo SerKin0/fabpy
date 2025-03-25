b9a652b77ac3f88bb1bfee80b499f609fced835a
SerKin0 <serget.skor007@gmail.com>
2025-03-25 22:38:08 +0300

    to pip

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="fabpy",
-    version="0.2",
+    version="0.4",
     packages=find_packages(),
-    description="Подобии библиотеки на Python для вычисления и оформления погрешностей в LaTeX для физических лабораторных работ.",
+    description="Python library for calculating and formatting errors in LaTeX in lab work.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="SerKin0",
